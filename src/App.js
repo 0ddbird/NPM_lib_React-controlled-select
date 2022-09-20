9c58893318080abf74b754411e029f810ea6f8b6
Giacomo Llinares <giacomo.llinares@gmail.com>
2022-09-20 18:57:20 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Select from './component/Select';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const options = [
+    {
+      name: 'test',
+      value: 'test'
+    },
+    {
+      name: 'test2',
+      value: 2
+    },{
+      name: 'test3',
+      value: [456, 'a', 39]
+    },
+  ]
+
+  const [selected, setSelected] = useState(options[0])
+
+  useEffect(() => {
+    console.log(selected)
+  }, [selected])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select options={options} selected={selected} setSelected={setSelected} />
     </div>
   );
 }
---
+++ src/component/Select.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { v4 as uuidv4 } from 'uuid'
+
+const Select = ({ options, selected, setSelected }) => {
+  function handleOptionSelection (event) {
+    const newSelection = options.find(object => object.value.toString() === event.target.value)
+    setSelected(newSelection)
+  }
+  return (
+    <select className='controlled-select' value={`${selected.value}`} onChange={(event) => handleOptionSelection(event)}>
+      { 
+        options.map( 
+          option => {
+            return <option key={uuidv4()} value={`${option.value}`}>{option.name}</option>
+          }
+        )
+      }
+    </select>
+  )
+}
+
+Select.propTypes = {
+  options: PropTypes.array,
+  selected: PropTypes.object,
+  setSelected: PropTypes.func
+}
+
+export default Select
